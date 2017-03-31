4e2f76c7e510e6ec5491fecad57d4294d9463dee
Arty-Maly <artem@brandeis.edu>
2017-03-31 10:11:44 -0400

    added css fade in and fixed json response

--- web/views/radio_station_view.ex
+++ web/views/radio_station_view.ex
@@ -3,7 +3,12 @@ defmodule VolnaApi.RadioStationView do
   use VolnaApi.Web, :view
 
   def render("index.json", %{radio_stations: radio_stations}) do
-    %{stations: Enum.map(radio_stations, &to_json/1)}
+     # = %{}
+    # %{stations: Enum.map(radio_stations, &to_json/1)}
+    json_map = Enum.reduce radio_stations, %{}, fn station, acc ->
+      Map.put(acc, station.name, to_json(station))
+    end
+    %{stations: json_map}
   end
 
   def to_json(radio_station) do
